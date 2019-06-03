d7c0880ae85a98f2c296c77d54c01ae28fa498c7
saud <saud@LAPTOP-SLM08CGF.routerc65343.com>
2019-06-03 19:38:15 +0300

    bug fixes

--- src/main/java/com/sacide/smart/home/api/compilation/DevicesManager.java
+++ src/main/java/com/sacide/smart/home/api/compilation/DevicesManager.java
@@ -11,6 +11,8 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,78 +20,104 @@ import java.util.Map;
  */
 public class DevicesManager {
 	
-	private int nextID = 0;
-	private HashMap<Integer, Device> devices = new HashMap<>();
-	
-	public DevicesManager() {
-		
+    private int nextID = 0;
+    private HashMap<Integer, Device> devices = new HashMap<>();
+
+    public DevicesManager() {
+
+    }
+
+    /**
+     * Adds a thread that refreshes the list of devices at the given interval.
+     * @param refreshDelay in seconds
+     */
+    public DevicesManager(double refreshDelay) {
+	int delayms = (int)(refreshDelay * 1000);
+	try {
+	    while(Thread.interrupted()) {
+		scanDevices();
+		Thread.sleep(delayms);
+	    }
+	} catch (InterruptedException ex) {
+	    Logger.getLogger(DevicesManager.class.getName()).log(Level.SEVERE, null, ex);
 	}
+    }
+
+    public Map<Integer, Device> getDeviceMap() {
+	return new HashMap<>(devices);
+    }
+
+    public Collection<Device> getDevices() {
+	return devices.values();
+    }
 	
-	public Map<Integer, Device> getDeviceMap() {
-		return new HashMap<>(devices);
+    /**
+     * Scans network for all available devices. Old devices keep their ID's
+     */
+    public final void scanDevices() {
+	HashSet<Device> newDevices = new HashSet<>();
+
+	LifxCommanderW lifx = new LifxCommanderW();
+	newDevices.addAll(lifx.discoverDevices());
+
+	BlindsAPI blinds = new BlindsAPI();
+	newDevices.addAll(blinds.discoverDevices());
+
+	PhilipsAPIV hue = new PhilipsAPIV(new File("./philips.prop"));
+	newDevices.addAll(hue.discoverDevices());
+
+	TPLinkAPI tplink = new TPLinkAPI();
+	newDevices.addAll(tplink.discoverDevices());
+
+	HashMap<Integer, Device> oldDevicesMap = new HashMap<>(devices);
+	HashSet<Device> oldDevicesSet = new HashSet<>(devices.values());
+	for(Device d : newDevices) {
+	    if(!oldDevicesSet.contains(d)) {
+		devices.put(nextID++, d);
+	    }
 	}
-	
-	public Collection<Device> getDevices() {
-		return devices.values();
+	for(Map.Entry<Integer, Device> entry : oldDevicesMap.entrySet()) {
+	    Integer key = entry.getKey();
+	    Device value = entry.getValue();
+	    if(!newDevices.contains(value)) {
+		devices.remove(key);
+	    }
 	}
+
+    }
 	
-	/**
-	 * Scans network for all available devices. Old devices keep their ID's
-	 */
-	public void scanDevices() {
-		HashSet<Device> newDevices = new HashSet<>();
-		
-		LifxCommanderW lifx = new LifxCommanderW();
-		newDevices.addAll(lifx.discoverDevices());
-		
-		BlindsAPI blinds = new BlindsAPI();
-		newDevices.addAll(blinds.discoverDevices());
-		
-		PhilipsAPIV hue = new PhilipsAPIV(new File("./philips.prop"));
-		newDevices.addAll(hue.discoverDevices());
-		
-		TPLinkAPI tplink = new TPLinkAPI();
-		newDevices.addAll(tplink.discoverDevices());
-		
-		HashMap<Integer, Device> oldDevicesMap = new HashMap<>(devices);
-		HashSet<Device> oldDevicesSet = new HashSet<>(devices.values());
-		for(Device d : newDevices) {
-			if(!oldDevicesSet.contains(d)) {
-				devices.put(nextID++, d);
-			}
-		}
-		for(Map.Entry<Integer, Device> entry : oldDevicesMap.entrySet()) {
-			Integer key = entry.getKey();
-			Device value = entry.getValue();
-			if(!newDevices.contains(value)) {
-				devices.remove(key);
-			}
-		}
-		
+    /**
+     * Gets a device object from its ID.
+     * @param id the ID.
+     * @return the requested device object or null if not found.
+     */
+    public Device getDevice(int id) {
+	return devices.get(id);
+    }
+
+    /**
+     * Gets a device's ID from its object.
+     * @param d the device.
+     * @return the requested device ID or -1 if not found.
+     */
+    public int getID(Device d) {
+	for(Map.Entry<Integer, Device> entry : devices.entrySet()) {
+	    Device value = entry.getValue();
+	    if(value.equals(d)) {
+		return entry.getKey();
+	    }
 	}
-	
-	/**
-	 * Gets a device object from its ID.
-	 * @param id the ID.
-	 * @return the requested device object or null if not found.
-	 */
-	public Device getDevice(int id) {
-		return devices.get(id);
+	return -1;
+    }
+
+    /**
+     * Adds a device to the list if it doesn't already exist.
+     * @param d The device to add.
+     */
+    public void addDevice(Device d) {
+	if(!devices.values().contains(d)) {
+	    devices.put(nextID++, d);
 	}
-	
-	/**
-	 * Gets a device's ID from its object.
-	 * @param d the device.
-	 * @return the requested device ID or -1 if not found.
-	 */
-	public int getID(Device d) {
-		for(Map.Entry<Integer, Device> entry : devices.entrySet()) {
-			Device value = entry.getValue();
-			if(value.equals(d)) {
-				return entry.getKey();
-			}
-		}
-		return -1;
-	}
-	
+    }
+    
 }
